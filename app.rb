3d6d4ae5dd81a163461533c7e538175012a2a473
Björn Engels <engels.bjorn@gmail.com>
2017-05-09 10:11:32 +0200

    optionparser documentation

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require_relative 'thermostaat.rb'
 
-main = Thermostaat.new
+foo = Thermostaat.new
 read = Input.new
 
 read.download
 change.compare(1,20)
+require_relative "thermostaat.rb"
+
+
+
+foo.ask_temperature
+foo.get_temperature
+foo.ask_max
+foo.get_max
+foo.ask_min
+foo.get_min
+foo.temperatuur
