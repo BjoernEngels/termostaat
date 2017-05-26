3cfa40fb2fd623588756a8920ede0698a72a1df6
Björn Engels <engels.bjorn@gmail.com>
2017-05-26 17:01:08 +0200

    v0.0.4 working

--- proces.rb
+++ proces.rb
@@ -8,10 +8,13 @@ class Proces
         @threshold = threshold
         if @downloaded_temperature < (threshold-range)
             @action = "verwarmen"
+            puts "RED"
         elsif @downloaded_temperature < (threshold+range)
             @action = "afkoelen"
+            puts "BLUE"
         else
             @action = "niets"
+            puts "GREEN"
 
     end
 end
--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 # Ruby Thermostaat
 
 #What does this project do
-nothing
+It reads the temperature from a remote server and displays it.
 
 #how to install it
 i dont know
 
 #example usage
-for a thermostat which doenn't need to work
+If you have a remote sensor which is connected to the internet, this app can display the temperature.
